8d2e126b6ecca18c83f21d74c9bca5cdb0270700
torokokill <torokokill@gmail.com>
2013-04-03 15:40:54 -0700

    Added status icons
    Added status icons to display the AI status of each turret.
    Threw in some comments
    TODO: Major display overhaul

--- TurretHelper/TurretHelper.lua
+++ TurretHelper/TurretHelper.lua
@@ -36,22 +36,27 @@ InterfaceOptions.AddMovableFrame({
 
 function OnLoad()
 	--Called when the addon loads
+	--Init the slash commands
 	InitSlashCommands();
-	
+	--Start the refresh timer, set to run every second
 	timer:Bind(UpdateAllDeployableTimeleft)
 	timer:Schedule(1)
 end
 
 function CreateDeployable(entityId)
+	--Creates a new deployable for insertion into the activeDeployables table
 	local info = Game.GetTargetInfo(entityId)
 	local abilityId = tonumber(info["sourceAbilityId"])
+	--Create a new widget from our template, and grabs important children widgets
 	local deployable = {group = Component.CreateWidget("deployableInfo", FRAME)};
 	deployable.icon = deployable.group:GetChild("icon");
 	deployable.icon:SetRegion(TRACKED_ENTITY_IDS[abilityId].icon)
 	deployable.healthbar = {group = deployable.group:GetChild("healthbar")};
 	deployable.healthbar.fg = deployable.healthbar.group:GetChild("hbFg");
 	deployable.timeLeft = deployable.group:GetChild("timeleft");
+	deployable.statusicon = deployable.group:GetChild("si");
 	
+	--Sets a duration on the entity if it has one
 	local duration = TRACKED_ENTITY_IDS[abilityId].duration;
 	if duration > 0 then
 		deployable.time = tonumber(System.GetClientTime()) + (duration * 1000)
@@ -70,38 +75,61 @@ function IsEntityTracked(entityId)
 end
 
 function UpdateDeployableInfo(entityId)
+	--Update the entity's info
 	local info = Game.GetTargetInfo(entityId);
 	--Do nothing, for now
 end
 
 function UpdateDeployableVitals(entityId)
+	--Update the entity's vitals
 	local vitals = Game.GetTargetVitals(entityId);
 	activeDeployables[entityId].healthbar.fg:SetDims("bottom:" .. (FRAME_SIZE-FRAME_BUFFER/2) .."; height:" .. tostring((FRAME_SIZE-FRAME_BUFFER)*vitals["health_pct"]) .. ";")
 end
 
 function UpdateDeployableStatus(entityId)
+	--Update the entity's status
 	local status = Game.GetTargetStatus(entityId);
-	local stateStr = "";
-	if status["state"] ~= nil then
-		stateStr = "[" .. status["state"] .. "]";
+	local state = status["state"]
+	if (state ~= nil) then
+		activeDeployables[entityId].statusicon:SetRegion(string.lower(tostring(state)))
 	end
 end
 
 function UpdateAllDeployableTimeleft()
+	--The function that the timer calls. Updates the time left on all deployables.
+	--Also prunes the deployable list
 	for i,j in pairs(activeDeployables) do
 		UpdateDeployableTimeleft(i);
+		UpdateDeployableStatus(i);
 	end
+	PruneDeployables()
 	timer:Reschedule(1)
 end
 
-function UpdateDeployableTimeleft(entityId)
+function GetTimeRemainingStr(entityId)
+	--Grabs the remaining time of a deployable and returns a formatted string in the form of MM:SS
 	if (activeDeployables[entityId].time > 0) then
-		activeDeployables[entityId].timeLeft:SetText(math.floor((tonumber(activeDeployables[entityId].time) - tonumber(System.GetClientTime())) / 1000));
+		local timeLeft = math.floor((tonumber(activeDeployables[entityId].time) - tonumber(System.GetClientTime())) / 1000);
+		local minutesLeft = math.floor(timeLeft / 60);
+		local secondsLeft = timeLeft % 60;
+		
+		if (secondsLeft < 10) then
+			secondsLeft = "0" .. secondsLeft;
+		end
+		
+		return minutesLeft .. ":" .. secondsLeft;
+	end
+end
+
+function UpdateDeployableTimeleft(entityId)
+	--Updates the deployable's remaining time if it has one
+	if (activeDeployables[entityId].time > 0) then
+		activeDeployables[entityId].timeLeft:SetText(GetTimeRemainingStr(entityId));
 	end
 end
 
 function UpdateDeployable(entityId)
-	--Update a deployable's info by entityId
+	--Update all of a deployable's info by entityId
 	UpdateDeployableInfo(entityId);
 	UpdateDeployableVitals(entityId);
 	UpdateDeployableStatus(entityId);
@@ -109,16 +137,17 @@ function UpdateDeployable(entityId)
 end
 
 function UpdateFramePositions()
+	--Iterates through all the activeDeployables, assigning them positions and grouping deployables from the same ability together
 	local idCount = {}
 	local currentRow = 0
 	for i,j in pairs(activeDeployables) do
 		local abilityId = tostring(j.abilityId)
 		if idCount[abilityId] == nil then
-			j.group:SetDims("left:0; top:" .. currentRow*(FRAME_SIZE+FRAME_BUFFER) .. ";")
+			j.group:SetDims("left:0; top:" .. currentRow*(FRAME_SIZE+FRAME_BUFFER+10) .. ";")
 			idCount[abilityId] = { count = 1, row = currentRow }
 			currentRow = currentRow + 1
 		else
-			j.group:SetDims("left:" .. (idCount[abilityId]["count"] * (FRAME_SIZE+FRAME_BUFFER)) .. "; top:" .. (idCount[abilityId]["row"]*(FRAME_SIZE+FRAME_BUFFER)) .. ";")
+			j.group:SetDims("left:" .. (idCount[abilityId]["count"] * (FRAME_SIZE+FRAME_BUFFER)) .. "; top:" .. (idCount[abilityId]["row"]*(FRAME_SIZE+FRAME_BUFFER+10)) .. ";")
 			idCount[abilityId]["count"] = idCount[abilityId]["count"] + 1
 		end
 	end
@@ -126,6 +155,8 @@ function UpdateFramePositions()
 end
 
 function UpdateAllDeployables()
+	--Iterates through all deployables assigned to a player
+	-- Adds new deployables that are tracked to the activeDeployables table and updates them
 	PruneDeployables()
 	local deployableList = Player.GetActiveDeployables();
 	for i = 1, #deployableList do
@@ -144,16 +175,24 @@ function UpdateAllDeployables()
 end
 
 function PruneDeployables()
+	--Checks to see if any deployables no longer exist. If they do not, remove them and update the positions of the active frames if any were removed
+	local removeDeployable = false
 	for i,k in pairs(activeDeployables) do
-		if Game.GetTargetStatus(i) == nil then
+		if (Game.GetTargetStatus(i) == nil) then
 			--["frame"]]:SetParam("alpha", 0.0)
 			Component.RemoveWidget(activeDeployables[i].group);
 			activeDeployables[i] = nil
+			removeDeployable = true
 		end
+	end
+	
+	if (removeDeployable) then
+		UpdateFramePositions()
 	end
 end
 
 function UpdateAbilities()
+	--Checks the equipped abilities of the player. Not called anywhere at the moment
 	local abilities = Player.GetAbilities();
 	for i,j in pairs(abilities["slotted"]) do
 		local ability = Player.GetAbilityInfo(j["abilityId"]);
@@ -162,18 +201,13 @@ function UpdateAbilities()
 end
 
 function OnMyAbilityDeployable(args)
+	--Called when the MY_ABILITY_DEPLOYABLE event is triggered
 	PruneDeployables()
 	UpdateAllDeployables();
 end
 
 function OnDeployableStatusChanged(args)
-	--[[local status = Game.GetTargetStatus(args["entityId"]);
-	local info = Game.GetTargetInfo(args["entityId"]);
-	local stateStr = "";
-	if status["state"] ~= nil then
-		stateStr = "[" .. status["state"] .. "]";
-	end]]--
-	--log("[StatusChanged] " .. info["name"] .. " " .. stateStr);
+	--Called when the ON_DEPLOYABLE_STATUS_CHANGED event is triggered
 	local entityId = tostring(args["entityId"])
 	if IsEntityTracked(entityId) then
 		UpdateDeployableStatus(entityId);
@@ -181,8 +215,7 @@ function OnDeployableStatusChanged(args)
 end
 
 function OnDeployableVitalsChanged(args)
-	--local vitals = Game.GetTargetVitals(args["entityId"]);
-	--log("[VitalsChanged] " .. tostring(vitals));--vitals["Health"] .. "/" .. vitals["MaxHealth"]);
+	--Called when the ON_DEPLOYABLE_VITALS_CHANGED event is triggered
 	local entityId = tostring(args["entityId"])
 	if IsEntityTracked(entityId) then
 		UpdateDeployableVitals(entityId);
@@ -190,8 +223,7 @@ function OnDeployableVitalsChanged(args)
 end
 
 function OnDeployableInfoChanged(args)
-	--local info = Game.GetTargetInfo(args["entityId"]);
-	--log("[InfoChanged] " .. " " .. info["name"]);
+	--Called when the ON_DEPLOYABLE_INFO_CHANGED event is triggered
 	local entityId = tostring(args["entityId"])
 	if IsEntityTracked(entityId) then
 		UpdateDeployableInfo(entityId);
@@ -199,17 +231,18 @@ function OnDeployableInfoChanged(args)
 end
 
 function OnCombatEvent(args)
+	--Called when the ON_COMBAT_EVENT event is triggered
 	local targetId = activeDeployables[tostring(args['TargetId'])]
 	local sourceId = activeDeployables[tostring(args['SourceId'])]
-	
+	log(tostring(args))
 	if (targetId ~= nil) or (sourceId ~= nil) then
 		print('a')
-		log(tostring(args))
+		--log(tostring(args))
 	end
 end
 
-
 function IsTrackedDeployable(entityId)
+	--Returns true if an entity's sourceAbilityId is one that we track
 	local id = Game.GetTargetInfo(entityId)
 	if id["sourceAbilityId"] ~= nil then
 		return TRACKED_ENTITY_IDS[tonumber(id["sourceAbilityId"])] ~= nil
@@ -222,6 +255,7 @@ function OnMessage()
 end
 
 function InitSlashCommands()
+	--Inits the slash commands
 	LIB_SLASH.BindCallback({slash_list = "th", description = "", func = TURRET_HELPER_SLASH_CMDS.cmdroot})
 end
 
